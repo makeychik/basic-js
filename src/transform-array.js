527cc87ead4a755721b77ad6ba65d51d7ac04409
makeychik <mikitamakeichyk@gmail.com>
2021-03-30 01:44:41 +0300

    fix: comment code of transform-array to submit in rsapp (fatal error while testing)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,43 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if (!Array.isArray(arr)) {
-    throw new Error("Argument is not array!");
-  }
+  // if (!Array.isArray(arr)) {
+  //   throw new Error("Argument is not array!");
+  // }
 
-  let result = [];
+  // let result = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    switch (arr[i]) {
-      case "--discard-next":
-        i++;
-        break;
-      case "--discard-prev":
-        if (i === 0 || arr[i - 2] === "--discard-next") {
-          break;
-        }
+  // for (let i = 0; i < arr.length; i++) {
+  //   switch (arr[i]) {
+  //     case "--discard-next":
+  //       i++;
+  //       break;
+  //     case "--discard-prev":
+  //       if (i === 0 || arr[i - 2] === "--discard-next") {
+  //         break;
+  //       }
 
-        result.pop();
-        break;
-      case "--double-next":
-        if (i === arr.length - 1) {
-          break;
-        }
+  //       result.pop();
+  //       break;
+  //     case "--double-next":
+  //       if (i === arr.length - 1) {
+  //         break;
+  //       }
 
-        result.push(arr[++i]);
-        break;
-      case "--double-prev":
-        if (i === 0 || arr[i - 2] === "--discard-next") {
-          break;
-        }
+  //       result.push(arr[++i]);
+  //       break;
+  //     case "--double-prev":
+  //       if (i === 0 || arr[i - 2] === "--discard-next") {
+  //         break;
+  //       }
 
-        result.push(arr[--i])
-        break;
-      default:
-        result.push(arr[i])
-        break;
-    }
-  }
+  //       result.push(arr[--i]);
+  //       break;
+  //     default:
+  //       result.push(arr[i]);
+  //       break;
+  //   }
+  // }
 
-  return result;
+  // return result;
 };
