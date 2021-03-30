3e2a25b416af2aa8890a8a436009d9effc284dd7
makeychik <mikitamakeichyk@gmail.com>
2021-03-30 15:51:19 +0300

    feat: implement solution of vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,68 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.direction = direction;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Invalid argument!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let encrypted = "";
+    let n = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        encrypted += String.fromCharCode(
+          ((message.charCodeAt(i) + key.charCodeAt(n % key.length) - 130) %
+            26) +
+            65
+        );
+        n++;
+      } else {
+        encrypted += message[i];
+      }
+    }
+
+    if (this.direction === false) {
+      return encrypted.split("").reverse().join("");
+    }
+
+    return encrypted;
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Invalid argument!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let decrypted = "";
+    let n = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        decrypted += String.fromCharCode(
+          ((message.charCodeAt(i) + 26 - key.charCodeAt(n % key.length)) % 26) +
+            65
+        );
+        n++;
+      } else {
+        decrypted += message[i];
+      }
+    }
+
+    if (this.direction === false) {
+      return decrypted.split("").reverse().join("");
+    }
+
+    return decrypted;
   }
 }
 
